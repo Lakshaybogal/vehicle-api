302c2c0c4cabcd92260fddf0fc55b4f44ce13159
Lakshay Rajput <rajputlakshay553@gmail.com>
2024-04-27 00:28:44 +0530

    just work now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
 from os import getenv
 if __name__ == '__main__' :
-    port  = getenv("PORT",8000)
+    port  = int(getenv("PORT",8000))
     uvicorn.run("app.main:app",host = "0.0.0.0",port = port,reload=True)
